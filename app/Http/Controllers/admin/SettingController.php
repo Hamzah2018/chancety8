e9d2c817de061e07e57adc76a5dcaa05d063fa01
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-09-10 17:06:49 +0300

    Curd: to setting table

---
+++ app/Http/Controllers/admin/SettingController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+use App\Models\Setting;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Storage;
+
+class SettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $settings = Setting::all();
+        return view('admin.setting',compact( 'settings'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        DB::beginTransaction();
+        $settings= new  Setting();
+        $settings->direct_post = $request->direct_post;
+        $settings->save();
+        DB::commit();
+        session()->flash('Add', 'تم اضافة البوست بنجاح ');
+        return redirect('/setting');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $id = $request->id;
+        $setting = Setting::find($id);
+        $setting->update([
+            'direct_post' => $request->direct_post
+        ]);
+        session()->flash('edit','تم تعديل البوست بنجاج');
+        return redirect('/setting');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        //
+        $id = $request->id;
+        Setting::find($id)->delete();
+        session()->flash('delete','تم حذف البوست بنجاح');
+        return redirect('setting');
+        // back()
+        // redirect('/home/dashboard');
+    }
+}
